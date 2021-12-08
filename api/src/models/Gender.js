e5d7dc7fc6f12e8df6e53ddf8a9d545ffec1da4b
c4chris124 <c4chris124@gmail.com>
2021-12-07 19:32:12 -0600

    Model Videogame & Gender completed

--- api/src/models/Gender.js
+++ api/src/models/Gender.js
@@ -3,29 +3,19 @@ const { DataTypes } = require('sequelize');
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
-  sequelize.define('videogame', {
+  sequelize.define('gender', {
     id:{
       type: DataTypes.UUID,
       allowNull: false,
       primaryKey: true
     },
-    image:{
-      type: DataTypes.STRING,
-      allowNull: false
-    },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
     },
-    description: {
-      type: DataTypes.STRING,
-    },
-    releaseDate: {
-      type: DataTypes.DATE,
-      defaultValue: DataTypes.NOW
-    },
-    rating: {
-      type: DataTypes.INTEGER
+    image_background: {
+        type: DataTypes.STRING,
+        allowNull: true
     }
   });
 };
