0f4ddffc5a6a222c20a17f074a5cc77fdefa8ef3
c4chris124 <c4chris124@gmail.com>
2021-12-15 13:41:23 -0600

    Adding redux devtools

--- client/src/store/index.jsx
+++ client/src/store/index.jsx
@@ -1,8 +1,8 @@
-import {applyMiddleware, createStore, compose} from 'redux'
+import {applyMiddleware, createStore} from 'redux'
+import {composeWithDevTools} from 'redux-devtools-extension'
 import thunk from 'redux-thunk'
 import reducer from './reducer'
 
-const store = createStore(reducer, applyMiddleware(thunk))
+const store = createStore(reducer, composeWithDevTools(applyMiddleware(thunk)))
 
 export default store
-// 397
