857bace2a0bdcde22da3410022c85c6835884768
c4chris124 <c4chris124@gmail.com>
2021-12-29 15:49:36 -0600

    Videogame Detail completed, adding pointer to filters

--- client/src/components/VideogameDetail/VideogameDetail.jsx
+++ client/src/components/VideogameDetail/VideogameDetail.jsx
@@ -1,14 +1,15 @@
-import React, {useEffect} from 'react'
-import {Link, useParams} from 'react-router-dom'
-import {useDispatch, useSelector} from 'react-redux'
-import {getGamesById, loadingAction} from '../../store/actions'
+import React, { useEffect } from 'react'
+import { Link, useParams } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
+import { getGamesById, loadingAction } from '../../store/actions'
 import LoadingGame from './LoadingGame'
- 
+import style from './VideogameDetail.module.css'
+
 function VideogameDetail() {
 
     const dispatch = useDispatch()
     let loading = useSelector((state) => state.loading)
-    const {id} = useParams()
+    const { id } = useParams()
 
     useEffect(() => {
         dispatch(getGamesById(id))
@@ -16,21 +17,26 @@ function VideogameDetail() {
     }, [dispatch])
 
     const myVideogame = useSelector((state) => state.videogame)
+
     return (
         <div>
             <Link to={'/videogames'}>Back</Link>
             {!loading ?
-            <div>
-                <img src={myVideogame.background_image} alt="" />
-                <h1>{myVideogame.name}</h1> 
-                <p>{myVideogame.description}</p>
-                <p>{myVideogame.released}</p>
-                <p>{myVideogame.rating}</p>
-                <p>{myVideogame.platforms}</p>
-                <p>{myVideogame.genres}</p>
-            </div>   
-            :  <LoadingGame/>
-        }
+                <div className={style.container}>
+                    <img src={myVideogame.background_image} alt="" />
+                    <div className={style.content}>
+                        <h1>{myVideogame.name}</h1>
+                        <p>{myVideogame.description}</p>
+                        <div className={style.content_detail}>
+                            <p><label>Released: </label>{myVideogame.released}</p>
+                            <p><label>Rating: </label>{myVideogame.rating}</p>
+                            <p><label>Platforms: </label>{myVideogame.platforms}</p>
+                            <p><label>Genres: </label>{myVideogame.genres}</p>
+                        </div>
+                    </div>
+                </div>
+                : <LoadingGame />
+            }
         </div>
     )
 }
